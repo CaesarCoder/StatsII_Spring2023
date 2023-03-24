0ba20d77c9e2b044f1f93818dcb52ac984bb36cf
CaesarCoder <zhangt4@tcd.ie>
2023-03-24 16:41:23 +0000

    Caesar's Answer

--- problemSets/PS03/My_Answer/PS3.R
+++ problemSets/PS03/My_Answer/PS3.R
@@ -1,12 +1,14 @@
-data <- read.csv("C:/Users/Caesar/Documents/GitHub/StatsII_Spring2023/datasets/gdpChange.csv", header = TRUE)
-
-
 library(MASS)
 library(nnet)
 library(ggplot2)
 library(stargazer)
 library(AER)
 library(pscl)
+
+
+data <- read.csv("C:/Users/Caesar/Documents/GitHub/StatsII_Spring2023/datasets/gdpChange.csv", header = TRUE)
+
+
 
 # set the level for outcome variable 
 
@@ -38,12 +40,52 @@ data2 <- read.csv("C:/Users/Caesar/Documents/GitHub/StatsII_Spring2023/datasets/
 
 
 mod.ps <- glm(PAN.visits.06 ~ competitive.district + marginality.06 + PAN.governor.06, data = data2, family = "poisson")
-summary(mod.ps)
+result <- summary(mod.ps)
 stargazer(mod.ps, title="mod.ps Results")
+
+cfs <- coef(mod.ps)
+cfs[2]   # coefficient for competitive.dsitrict 
+
+se_cd <- result$coefficients[2, 2]
+se_cd  # standard error of competitve.district
+
+## I will conduct a Z test to test individual coefficient for the variable Competitive.district
+## I set 95% as our confidence interval
+## H0: B1 = 0 
+## Ha: B0 not equals to 0 
+## Z test score: 
+z = (cfs[2]-0)/se_cd
+z # -0.4766106
+
+# two-tail, p-value
+p = 2*pnorm(-0.4766106, lower.tail=FALSE)
+p
+# p value: 1.366, which is larger than 5%. Therefore, we cannot rejct the null hypothesis that
+# the coefficient for the competitive.district equals to 0.
+
 
 
 # b) 
 
+
+# format: 
+# for a one unit change in the predictor variable, the difference in the logs of
+# expected counts is expected to change by the respective regression coefficient,
+# given the other predictor variables in the model are held constant.
+
+
+
+# interpretation of the coefficient for marginality.06: 
+# Holding all other covariates constant, for a one unit change in marginality.06, 
+# from the category of not-marginal to the category of marginal, the logs of 
+# expected counts of visit is expected to decrease by 2.080. 
+
+
+
+# interpretation of the coefficient for PAN.governor.06:  
+# Holding all other covariates constant,for a one unit change in PAN.governor.06,
+# from the category of not PAN.governor to the category of PAN.governor, the logs 
+# of expected counts of visit is expected to decrease by 0.312. 
 
 
 # c)
@@ -58,9 +100,8 @@ predict(mod.ps, newdata= pred, type = "response")
 
 
 
-cfs <- coef(mod.ps)
-cfs
+exp(cfs[1] + cfs[2] * 1 + cfs[3] * 0 + cfs[4] *1)
 
-exp(cfs[1] * 1 + cfs[2] * 0 + cfs[3] *1)
+##  0.01494818 
 
-## 0.002765929 
+## In conclusion, the estimated mean number of visit is 0. 
